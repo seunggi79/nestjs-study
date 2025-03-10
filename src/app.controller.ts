655c9e364869c5d0e8006763c1d17d99c6ba722b
seunggi <hsg77689469@gmail.com>
2025-03-11 08:55:26 +0900

    ConfigModule

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,13 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello() {
     return this.appService.getHello();
+  }
+
+  @Post()
+  @HttpCode(204)
+  create(){
+    return 'this action adds a new cat'
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly configService: ConfigService) {}
+
+  getHello() {
+    // process.env.DB_PASSWORD 와 같다다
+    return this.configService.get('DB_PASSWORD')
+    
   }
 }
