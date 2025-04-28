f245ecec1cd4ecf7d60216bffa7dfb46fac6717a
seunggi <hsg77689469@gmail.com>
2025-04-29 08:02:31 +0900

    guard @nestjs/passport

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Get, Post, Req, Res, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, Res, UseGuards, UseInterceptors } from '@nestjs/common';
 import { JoinRequestDto } from './dto/join.request.dto';
 import { UsersService } from './users.service';
 import { ApiOkResponse, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { UserDto } from 'src/common/dto/user.dto';
 import { User } from 'src/common/decorator/user.decorator';
 import { UndefinedToNullInterceptor } from 'src/common/interceptor/undefinedToNull.interceptor';
+import { LocalAuthGuard } from 'src/auth/local-auth.guard';
 
 @UseInterceptors(UndefinedToNullInterceptor) // 인터셉터 연결
 @Controller('api/users')
@@ -33,12 +34,17 @@ export class UsersController {
         await this.userService.join(body.email, body.nickname, body.password)
     }
 
-    @ApiOkResponse({
-        type: UserDto
-    })
-    @Post('login')
-    logIn() {
 
+    @ApiResponse({
+        status: 200,
+        description : '성공',
+        type : UserDto,
+    })
+    @ApiOperation({summary:'로그인'})
+    @UseGuards(LocalAuthGuard)
+    @Post('login')
+    logIn(@User() user) {
+        return user;
     }
 
     @Post('logout')
